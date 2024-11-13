de388d5f72a2f8e813c7a3ec8b190e1fd08a6330
Mahmoud Mohamed <mahmoudmohamedm82@gmail.com>
2024-11-13 16:11:07 +0200

    bg2

--- src/components/Route/Hero/Hero.jsx
+++ src/components/Route/Hero/Hero.jsx
@@ -9,8 +9,8 @@ const Hero = () => {
     <div
       className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
       style={{
-        backgroundImage: "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)"
-        // `url(${bg})`,
+        backgroundImage: `url(${bg})`,
+        
       }}
     >
       <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
